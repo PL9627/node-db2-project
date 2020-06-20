8a486ec953fd42cf3f535d0507b2ce5145b48fa3
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-19 17:37:13 -0700

    created welcomeRouter and imported into index.js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const express = require("express")
-const cors = require("cors")
-const helmet = require("helmet")
-const carRouter = require("./routers/car-router")
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const carRouter = require("./routers/car-router");
+const welcomeRouter = require("./routers/welcome-router");
 
-const server = express()
-const port = process.env.PORT || 3200
+const server = express();
+const port = process.env.PORT || 3200;
 
-server.use(helmet())
-server.use(express.json())
-server.use(cors)
+server.use(helmet());
+server.use(express.json());
+server.use(cors);
 
-server.use("/cars", carRouter)
+server.use("/", welcomeRouter);
+server.use("/cars", carRouter);
 
 server.use((err, req, res, next) => {
-	console.log(err)
-	res.status(500).json({
-		message: "Something went wrong",
-	})
-})
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong",
+  });
+});
 
 server.listen(port, () => {
-	console.log(`Running at http://localhost:${port}`)
-})
+  console.log(`Running at http://localhost:${port}`);
+});
