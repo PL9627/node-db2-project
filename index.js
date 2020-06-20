871c7f04a1444b66ee1b0e776664910ad72f0e86
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-19 17:35:00 -0700

    set up api call skeletons and finished index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+const carRouter = require("./routers/car-router")
+
+const server = express()
+const port = process.env.PORT || 3200
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors)
+
+server.use("/cars", carRouter)
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Running at http://localhost:${port}`)
+})
--- routers/car-router.js
+++ routers/car-router.js
@@ -3,4 +3,16 @@ const db = require("../data/config");
 
 const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+
+})
+
+router.get("/:id", async (req, res, next) => {
+
+})
+
+router.post("/", async (req, res, next) => {
+
+})
+
 module.exports = router;
