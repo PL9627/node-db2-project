09154ebaf69b01fad63e0912d9e4068f996d4ee0
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-19 17:06:25 -0700

    finished the knex up schema in migrations carDealer

---
+++ migrations/20200619165425_carDealer.js
@@ -0,0 +1,13 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable("cars", (table) => {
+    table.increments("id");
+    table.real("VIN").notNull().unique();
+    table.text("make").notNull();
+    table.text("model").notNull();
+    table.real("mileage").notNull();
+    table.text("transmissionType");
+    table.text("titleStatus");
+  });
+};
+
+exports.down = function (knex) {};
