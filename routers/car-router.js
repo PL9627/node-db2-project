3b678a541c23b32b2123ddebab7c7b53ae476652
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-19 17:38:46 -0700

    finished GET / call

--- routers/car-router.js
+++ routers/car-router.js
@@ -4,15 +4,29 @@ const db = require("../data/config");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
+    try {
+        const cars = await db("cars")
 
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.get("/:id", async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.post("/", async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = router;
